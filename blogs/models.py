63fd66aca24bf0c588198418dc39c519c8cc53ed
kushal kc <kckushal164@gmail.com>
2026-01-25 21:39:29 +0545

    Initializint Category model

---
+++ blogs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=100,unique=True)
+  created_at=models.DateTimeField(auto_now_add=True)
+  updated_at=models.DateTimeField(auto_now=True)
+
+  class Meta:
+    verbose_name = "Category"
+    verbose_name_plural = "Categories"
