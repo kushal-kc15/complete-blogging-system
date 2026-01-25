0ad22305279b5edfe4e352f5436d3dda5d9eed83
kushal kc <kckushal164@gmail.com>
2026-01-25 22:03:29 +0545

    Admin Table List Display

--- blogs/admin.py
+++ blogs/admin.py
@@ -7,6 +7,9 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category,CategoryAdmin)
 
 class BlogAdmin(admin.ModelAdmin):
-  list_display = ['title','category','author','created_at','updated_at']
+  list_display = ['title','category','author','created_at','updated_at','is_featured','status']
   prepopulated_fields = {'slug':('title',)}
+  search_fields = ['id','title','category__name','status']
+  list_filter = ['title','category','created_at','updated_at','is_featured','status']
+  list_editable = ['is_featured','status']
 admin.site.register(Blog,BlogAdmin)
