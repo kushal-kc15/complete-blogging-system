07bdaf92f90a0967d6fd037a7140b16e137be202
kushal-kc15 <kckushal164@gmail.com>
2026-07-23 23:38:28 +0545

    ..

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, reverse_lazy
+from django.urls import path, include, reverse_lazy, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -85,4 +86,5 @@ urlpatterns = [
     path('categories/', BlogsView.category_index, name='category_index'),
     path('<slug:slug>/', BlogsView.BlogDetail, name='Blog_detail'),
     path('blogs/search/', BlogsView.Search, name='search'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
