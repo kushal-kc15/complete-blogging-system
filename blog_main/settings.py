0ab69cfa4887c60f666d88f0587fba36bd938999
kushal-kc15 <kckushal164@gmail.com>
2026-07-23 16:25:35 +0545

    dd AccountMiddleware for django-allauth

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'blog_main.urls'
