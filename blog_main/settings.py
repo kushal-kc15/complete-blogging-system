3829ccd5639b6f102bcb6ddb05935f115a0d7f61
kushal-kc15 <kckushal164@gmail.com>
2026-07-23 16:56:01 +0545

    ...

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -126,7 +126,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
